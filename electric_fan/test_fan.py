5d495fff2c8ae8583034060a2ec3ab192f60bf97
USER <eugenemabansag8@gmail.com>
2026-06-04 23:46:52 +0800

    Added construct and all private variables

--- electric_fan/test_fan.py
+++ electric_fan/test_fan.py
@@ -1,11 +1,20 @@
 # Assign a class named Fan
-## three constants named SLOW, MEDIUM AND FAST
+class Fan:
+    ## construct for creating fan with the specified speed, radius, color and on/off
+    def __init__(self, speed, radius, color, on_off):
+    ## three constants named SLOW, MEDIUM AND FAST
+        SLOW = 1
+        MEDIUM = 2
+        FAST = 3
 ## private int named speed for the speed of the fan
+        __speed = speed
 ## private bool to know if the fan is on or off
+        __on_status = on_off
 ## private float to identify fan radius
+        __radius = radius
 ## private string to identify fan color
+        __color = color
 ## Accessor and Mutator method for all four data fields
-## construct for creating fan with the specified speed, radius, color and on/off
 ### Test two fan objects
 #### maximum speed, radius 10, color yellow and turn on
 #### Medium speed, radius 5, rolor blue and turned off
