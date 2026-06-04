8e53fc49287b287d60efdfc50319f6a424f14060
USER <eugenemabansag8@gmail.com>
2026-06-04 23:40:38 +0800

    Added pseudocode

---
+++ electric_fan/test_fan.py
@@ -0,0 +1,11 @@
+# Assign a class named Fan
+## three constants named SLOW, MEDIUM AND FAST
+## private int named speed for the speed of the fan
+## private bool to know if the fan is on or off
+## private float to identify fan radius
+## private string to identify fan color
+## Accessor and Mutator method for all four data fields
+## construct for creating fan with the specified speed, radius, color and on/off
+### Test two fan objects
+#### maximum speed, radius 10, color yellow and turn on
+#### Medium speed, radius 5, rolor blue and turned off
