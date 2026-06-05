ac2dc573167e443c0d2f0c725223ac30624e63c2
USER <eugenemabansag8@gmail.com>
2026-06-05 09:19:28 +0800

    Added pseudocode

---
+++ car/car_object.py
@@ -0,0 +1,6 @@
+# Class Car with year model, make and speed
+## init method that takes year and make as arguments assigned to __year_model and __make, set default speed to 0
+## accelerate method to add 5 to speed
+## brake method to subtract 5 to speed
+## get_speed method to return speed
+### Car object : Accelerate 5 times and brake 5 times
