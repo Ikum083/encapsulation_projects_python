38f56c977969ffc7d555dfec12e261d634ccc26f
USER <eugenemabansag8@gmail.com>
2026-06-05 09:22:36 +0800

    Added car class with init method

--- car/car_object.py
+++ car/car_object.py
@@ -1,5 +1,10 @@
 # Class Car with year model, make and speed
+class Car:
 ## init method that takes year and make as arguments assigned to __year_model and __make, set default speed to 0
+    def __init__(self, year, make):
+        self.__year_model = year
+        self.__make = make
+        self.__speed = 0
 ## accelerate method to add 5 to speed
 ## brake method to subtract 5 to speed
 ## get_speed method to return speed
