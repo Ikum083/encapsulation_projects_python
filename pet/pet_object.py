4bc28b9c63fffdbce65d2e6da4853d00cf0ecaff
USER <eugenemabansag8@gmail.com>
2026-06-05 09:40:05 +0800

    Added pet folder for pet object

---
+++ pet/pet_object.py
@@ -0,0 +1,5 @@
+# create pet class with __name, __animal_type and __age
+## init method that contains the name, animal type and age variables
+## set name, animal type and age
+## get name, animal type and age
+### prompt user to input their pet's attributes
