786d752176fa0c40d0203841f60f1fe6d0c82669
USER <eugenemabansag8@gmail.com>
2026-06-05 08:46:32 +0800

    Added mutators for each data field

--- electric_fan/test_fan.py
+++ electric_fan/test_fan.py
@@ -14,14 +14,33 @@ class Fan:
         self.__radius = radius
 ## private string to identify fan color
         self.__color = color
+
+    # accessor
     def get_speed(self):
         return self.__speed
+    
     def get_radius(self):
         return self.__radius
+    
     def get_color(self):
         return self.__color
+    
     def get_on_status(self):
         return self.__on_status
+        
+    # mutator
+    def set_speed(self, speed):
+        self.__speed = speed
+    
+    def set_speed(self, radius):
+        self.__radius = radius
+
+    def set_speed(self, color):
+        self.__color = color
+
+    def set_speed(self, on_off):
+        self.__on_status = on_off
+    
 fan = Fan(speed = 3, radius = 10, color = "yellow", on_off = "on")
 ### Test two fan objects
 #### maximum speed, radius 10, color yellow and turn on
