08fa693ca28d6a64f68bc7fd93e83774a5cb00b5
USER <eugenemabansag8@gmail.com>
2026-06-05 09:46:20 +0800

    Added accessor and mutator methods

--- pet/pet_object.py
+++ pet/pet_object.py
@@ -5,6 +5,25 @@ class Pet:
         self.__name = name
         self.__animal_type = animal_type
         self.__age == age
+    
 ## set name, animal type and age
+    def set_name(self, name):
+        self.__name = name
+
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    def set_age(self, age):
+        self.__age = age
+
 ## get name, animal type and age
+    def get_name(self):
+        return self.__name
+    
+    def get_age(self):
+        return self.__age
+    
+    def get_animal_type(self):
+        return self.__animal_type
+    
 ### prompt user to input their pet's attributes
