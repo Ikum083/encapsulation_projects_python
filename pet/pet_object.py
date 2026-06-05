e983c2e69350f11c97001d5ec145c1971a4efe5b
USER <eugenemabansag8@gmail.com>
2026-06-05 09:42:48 +0800

    Added pet class with init method to contain name, animal type and age attributes

--- pet/pet_object.py
+++ pet/pet_object.py
@@ -1,5 +1,10 @@
 # create pet class with __name, __animal_type and __age
+class Pet:
 ## init method that contains the name, animal type and age variables
+    def __init__(self, name, animal_type, age):
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age == age
 ## set name, animal type and age
 ## get name, animal type and age
 ### prompt user to input their pet's attributes
